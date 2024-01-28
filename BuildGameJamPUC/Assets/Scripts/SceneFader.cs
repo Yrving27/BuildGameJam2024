a0c7db7da5f37bb54512641977b7aff5a39835ba
João Victor <95589680+7jvl@users.noreply.github.com>
2024-01-28 15:43:27 -0300

    retiramos a tela de load
    não vai dar tempo de finalizar

---
+++ BuildGameJamPUC/Assets/Scripts/SceneFader.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEditor.SearchService;
+
+public class SceneFader : MonoBehaviour {
+
+	public Image img;
+	public AnimationCurve curve;
+
+	void Start ()
+	{
+		StartCoroutine(FadeIn());
+	}
+
+    private void OnLevelWasLoaded()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeTo(int scene)
+	{
+		Debug.Log("Fade to " + scene);
+		StartCoroutine(FadeOut(scene));
+	}
+
+	public void SkipTime()
+	{
+		StartCoroutine(AltFadeOut());
+	}
+
+	IEnumerator FadeIn ()
+	{
+		float t = 1f;
+
+		while (t > 0f)
+		{
+			t -= Time.deltaTime;
+			float a = curve.Evaluate(t);
+			img.color = new Color (0f, 0f, 0f, a);
+			yield return 0;
+		}
+	}
+
+	IEnumerator FadeOut(int scene)
+	{
+		float t = 0f;
+
+		while (t < 1f)
+		{
+			t += Time.deltaTime;
+			float a = curve.Evaluate(t);
+			img.color = new Color(0f, 0f, 0f, a);
+			yield return 0;
+		}
+			
+		SceneManager.LoadScene(scene);
+	}
+
+	IEnumerator AltFadeOut()
+	{
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+			yield return 0;
+        }
+
+		yield return new WaitForSeconds(.5f);
+		StartCoroutine(FadeIn());
+    }
+}
